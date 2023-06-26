90443977b96d752a74b53303cd515fee0807dfba
Informatika <veljkojovkovic1999@gmail.com>
2023-06-26 18:04:06 +0200

    2. commit

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -7,10 +7,76 @@ import pages.HomePage;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void successfullLogin(){
+    public void standardUserLogin() {
         loginPage.setUsername("standard_user");
         loginPage.setPassword("secret_sauce");
         HomePage homePage = loginPage.clickLoginButton();
-        Assert.assertEquals(homePage.getTitle(), "Products", "Not expecte");
+        Assert.assertEquals(homePage.getTitle(), "Products", "Not expected");
+    }
+
+    @Test
+    public void unsuccessfullLogin1() {
+        loginPage.setUsername("Veljko");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        Assert.assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Username and password do not match any user in this service",
+                "User is login successfully");
+    }
+
+    @Test
+    public void unsuccessfullLogin2() {
+        loginPage.setUsername("standard_user");
+        loginPage.setPassword("Veljko");
+        loginPage.clickLoginButton();
+        Assert.assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Username and password do not match any user in this service",
+                "User is login successfully");
+    }
+
+    @Test
+    public void problemUserLogin() {
+        loginPage.setUsername("problem_user");
+        loginPage.setPassword("secret_sauce");
+        HomePage homePage = loginPage.clickLoginButton();
+        Assert.assertEquals(homePage.getTitle(), "Products", "Not expected");
+    }
+
+    @Test
+    public void lockedUsedLogin() {
+        loginPage.setUsername("locked_out_user");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        Assert.assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Sorry, this user has been locked out.",
+                "User is login successfully");
+    }
+
+    @Test
+    public void performanceGlitchUserLogin() {
+        loginPage.setUsername("performance_glitch_user");
+        loginPage.setPassword("secret_sauce");
+        HomePage homePage = loginPage.clickLoginButton();
+        Assert.assertEquals(homePage.getTitle(), "Products", "Not expected");
+    }
+
+    @Test
+    public void loginJustWithUsername() {
+        loginPage.setUsername("standard_user");
+        loginPage.setPassword("");
+        loginPage.clickLoginButton();
+        Assert.assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Password is required",
+                "User is login successfully");
+    }
+    @Test
+    public void loginJustWithPassword() {
+        loginPage.setUsername("");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        Assert.assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Username is required",
+                "User is login successfully");
     }
 }
+
